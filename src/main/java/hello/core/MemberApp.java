bcaa4312273764ec8bf093e5a971c5e53e1bf973
myzi726 <mzi726@naver.com>
2023-08-08 21:43:54 +0900

    feat AppConfig

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,14 +3,13 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-
-import java.sql.SQLOutput;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService(); //memberService에는 memberServiceImpl이 있음
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
